10780df6bba69c2f5172979d09ce9b26d6301fa4
washupro <washup72@gmail.com>
2025-02-14 14:52:19 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const buttonNo = document.querySelector('#no')
+const buttonYes = document.querySelector('#yes')
+
+let fontSize = 2
+
+let messages = [
+  'Estas segura?',
+  'Piensalo bien',
+  'Piensalo muy bien',
+  'Piensalo',
+  'Mira el otro botón',
+  'Otra oportunidad',
+  'Una oportunidad mas',
+  'Ya sabes como terminar esto'
+]
+
+buttonNo.addEventListener('click', () => {
+  fontSize = fontSize + .5
+  buttonYes.style.fontSize = `${fontSize}rem`
+
+  const indexRandom = Math.floor(Math.random() * messages.length)
+
+  buttonNo.textContent = messages[indexRandom]
+})
+
+buttonYes.addEventListener('click', () => {
+  document.querySelector('#message').style.display = 'flex'
+})
